00c2d48df65be11a7f288cb139e6d457129b7086
pkthunder87 <pkthunder87@gmail.com>
2023-09-10 21:30:37 -0700

    Lesson 287: Fetching Data With React Router "Loaders": Pizza Menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant.js";
+import MenuItem from "./MenuItem.jsx";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  console.log(menu);
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
