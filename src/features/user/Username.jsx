481df04b77c99a0297fb8e91467c098356b49ef6
pkthunder87 <pkthunder87@gmail.com>
2023-09-11 22:06:10 -0700

    Lesson 300: Using Flexbox

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="font-se hidden text-sm">Paul</div>;
+  return <div className="font-se hidden text-sm md:block">Paul</div>;
 }
 
 export default Username;
