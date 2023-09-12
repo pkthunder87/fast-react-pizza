dd94e911c1698a971693bd65cf5a18dfbe44469c
pkthunder87 <pkthunder87@gmail.com>
2023-09-11 20:39:06 -0700

    Lesson 296: Working With Color

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from "../features/order/SearchOrder.jsx";
 
 function Header() {
   return (
-    <header>
+    <header className="bg-yellow-500">
       <Link to="/">Fast React Pizza Co.</Link>
 
       <SearchOrder />
