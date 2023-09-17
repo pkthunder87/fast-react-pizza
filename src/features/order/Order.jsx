b7d30d8732655b9d23d9635fddc9f085800df6b4
pkthunder87 <pkthunder87@gmail.com>
2023-09-16 21:20:53 -0700

    Lesson 325: Updating Data Without Navigation

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -9,6 +9,7 @@ import {
 } from '../../utils/helpers';
 import OrderItem from './OrderItem.jsx';
 import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder.jsx';
 
 function Order() {
   const order = useLoaderData();
@@ -21,8 +22,6 @@ function Order() {
     },
     [fetcher],
   );
-
-  console.log(fetcher.data);
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -91,6 +90,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
